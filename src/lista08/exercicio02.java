658186756ca8a37eab414a4dc1329b5646a4785c
Patryck Gratão <ppgratao@gmail.com>
2019-11-11 20:53:53 -0300

    atualizando exercicio02.java

--- src/lista08/exercicio02.java
+++ src/lista08/exercicio02.java
@@ -1,45 +1,52 @@
-/*
- * Desenvolva um programa que leia duas matrizes (2 x 3 e 3 x 2) do tipo inteiro.
-Efetue a multiplicação da matriz e apresente a matriz resultante. 
- * 
- * */
-
-package lista08;
-
 import javax.swing.JOptionPane;
 
+/*
+ * 2. Desenvolva um programa que leia duas matrizes (2 x 3 e 3 x 2) do tipo inteiro.
+ Efetue a multiplicaÃ§Ã£o da matriz e apresente a matriz resultante. 
+ * 
+ * */
 public class exercicio02 {
 	public static void main(String[] args) {
-		double matrizA[][] = new double[2][3];
-		double matrizB[][] = new double[3][2];
-		double resultante[][] = new double[2][2];
+		int somatorio;
+		int[][] matrizA = new int[2][3];
+		int[][] matrizB = new int[3][2];
+		int[][] matrizC = new int[2][2];
 
-		// Leitura da Matriz A
+		// Preenchimento Matriz A
+		System.out.println("Matriz A");
 		for (int i = 0; i < 2; i++) {
 			for (int j = 0; j < 3; j++) {
-				matrizA[i][j] = Double.parseDouble(JOptionPane
-						.showInputDialog("Matriz A\nDigite o valor da posição [ " + (i + 1) + ", " + (j + 1) + "]"));
+				matrizA[i][j] = Integer.parseInt(JOptionPane
+						.showInputDialog("Valor ["+i+","+j+"]"));
 			}
 		}
 
-		// Leitura da Matriz B
+		// Preenchimento Matriz B
+		System.out.println("Matriz B");
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 2; j++) {
-				matrizB[i][j] = Double.parseDouble(JOptionPane
-						.showInputDialog("Matriz B\nDigite o valor da posição [ " + (i + 1) + ", " + (j + 1) + "]"));
+				matrizB[i][j] = Integer.parseInt(JOptionPane
+						.showInputDialog("Valor ["+i+","+j+"]"));
+			}
+		}
+		// LÃ³gica do Programa
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 2; j++) {
+				somatorio = 0;
+				for (int k = 0; k < 2; k++) {
+					somatorio = somatorio + (matrizA[i][k] * matrizB[k][j]);
+				}
+				matrizC[i][j] = somatorio;
 			}
 		}
 
-		// Lógica do Programa
-		//for (int i = 0; i < 3 )
-	
-		// Impressões
 		for (int i = 0; i < 2; i++) {
 			for (int j = 0; j < 2; j++) {
-				System.out.printf("  %2f",resultante[i][j]);
+				System.out.print(matrizC[i][j]+", ");
 			}
-			System.out.println("\n");
+			System.out.println();
 		}
 
 	}
+
 }
